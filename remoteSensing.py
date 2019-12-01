8070526c4586a15491e200ae0257ee8975b13ff6
Saul <shaul.s@campus.technion.ac.il>
2019-12-01 16:34:29 +0200

    reading ascii file

--- remoteSensing.py
+++ remoteSensing.py
@@ -2,4 +2,15 @@ import matplotlib as plt
 import numpy as np
 
 
-print('hello world')
+try:
+    raw_data = open(r'C:\Users\Saul\OneDrive - Technion\סמסטר 5\חישה\מעבדה 2\הארה אנכית\alon_0000.asc', 'r', encoding='utf8')
+except:
+    print('Could not open file')
+    quit()
+
+data = raw_data.readlines()
+
+print(data[0])
+
+
+#if __name__ == "__main__":
