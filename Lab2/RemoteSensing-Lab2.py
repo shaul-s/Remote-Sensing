40aa2588516560ecdd2ba1cb100d637cf02f60df
Saul <Saul@DESKTOP>
2019-12-02 21:52:19 +0200

    better graphs

--- Lab2/RemoteSensing-Lab2.py
+++ Lab2/RemoteSensing-Lab2.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import glob
+import re
 
 
 """
@@ -50,6 +51,7 @@ for idx, file in enumerate(fileNames[::3]):
     plt.plot(wavelength1, reflectance1, 'r-', label='1')
     plt.plot(wavelength2, reflectance2, 'g-', label='2')
     plt.plot(wavelength3, reflectance3, 'b-', label='3')
+    plt.legend()
     plt.savefig("vertical_spectral_reflectance" + str(idx) + ".png", format="PNG", dpi=150)
     plt.cla()
 
@@ -85,6 +87,15 @@ getting the data from 30 degree light source
 """
 fileNames = glob.glob(r'30_angle_radiance\*.asc')
 
+fig1, ax1 = plt.subplots(figsize=(9, 7), dpi=150, facecolor='w', edgecolor='k')
+ax1.set_ylim(-50, 150)
+ax1.set_title('Average Reflectance for all Objects')
+ax1.grid()
+ax1.set_xlabel('Wavelength[nm]')
+ax1.set_ylabel('Reflectance[%]')
+
+fig2, ax2 = plt.subplots(figsize=(9, 7), dpi=150, facecolor='w', edgecolor='k')
+
 for idx, file in enumerate(fileNames[::3]):
     try:
         raw_data = open(str(file), 'r', encoding='utf8')
@@ -118,44 +129,54 @@ for idx, file in enumerate(fileNames[::3]):
     """
     plotting
     """
-    plt.figure(num=None, figsize=(9, 7), dpi=150, facecolor='w', edgecolor='k')
-    plt.ylim(-50, 150)
-    plt.title(file)
-    plt.grid()
-    plt.xlabel('Wavelength[nm]')
-    plt.ylabel('Reflectance[%]')
-    plt.plot(wavelength1, reflectance1, 'r-', label='1')
-    plt.plot(wavelength2, reflectance2, 'g-', label='2')
-    plt.plot(wavelength3, reflectance3, 'b-', label='3')
-    plt.savefig("30deg_spectral_reflectance" + str(idx) + ".png", format="PNG", dpi=150)
-    plt.cla()
+    #plt.figure(num=None, figsize=(9, 7), dpi=150, facecolor='w', edgecolor='k')
+    ax2.set_ylim(-50, 150)
+    ax2.set_title(file)
+    ax2.grid()
+    ax2.set_xlabel('Wavelength[nm]')
+    ax2.set_ylabel('Reflectance[%]')
+    ax2.plot(wavelength1, reflectance1, 'r-', label='1')
+    ax2.plot(wavelength2, reflectance2, 'g-', label='2')
+    ax2.plot(wavelength3, reflectance3, 'b-', label='3')
+    ax2.legend()
+    fig2.savefig("30deg_spectral_reflectance" + str(idx) + ".png", format="PNG", dpi=150)
+    ax2.cla()
 
-    plt.ylim(-50, 150)
-    plt.title('Average Reflectance ' + file)
-    plt.grid()
-    plt.xlabel('Wavelength[nm]')
-    plt.ylabel('Reflectance[%]')
-    plt.plot(wavelength1, reflectance_avg, 'k')
-    plt.savefig("30deg_spectral_reflectance_avg" + str(idx) + ".png", format="PNG", dpi=150)
-    plt.cla()
+    ax2.set_ylim(-50, 150)
+    ax2.set_title('Average Reflectance ' + file)
+    ax2.grid()
+    ax2.set_xlabel('Wavelength[nm]')
+    ax2.set_ylabel('Reflectance[%]')
+    ax2.plot(wavelength1, reflectance_avg, 'k')
+    fig2.savefig("30deg_spectral_reflectance_avg" + str(idx) + ".png", format="PNG", dpi=150)
+    ax2.cla()
 
-    plt.ylim(-50, 150)
-    plt.title('Standard Deviation - Reflectance ' + file)
-    plt.grid()
-    plt.xlabel('Wavelength[nm]')
-    plt.ylabel('Reflectance[%]')
-    plt.plot(wavelength1, reflectance_std, 'k')
-    plt.savefig("30deg_spectral_reflectance_std" + str(idx) + ".png", format="PNG", dpi=150)
-    plt.cla()
+    ax2.set_ylim(-50, 150)
+    ax2.set_title('Standard Deviation - Reflectance ' + file)
+    ax2.grid()
+    ax2.set_xlabel('Wavelength[nm]')
+    ax2.set_ylabel('Reflectance[%]')
+    ax2.plot(wavelength1, reflectance_std, 'k')
+    fig2.savefig("30deg_spectral_reflectance_std" + str(idx) + ".png", format="PNG", dpi=150)
+    ax2.cla()
 
-    plt.ylim(-50, 150)
-    plt.title('(Max Obs - Min Obs) - Reflectance ' + file)
-    plt.grid()
-    plt.xlabel('Wavelength[nm]')
-    plt.ylabel('Reflectance[%]')
-    plt.plot(wavelength1, reflectance_neg, 'k')
-    plt.savefig("30deg_spectral_reflectance_neg" + str(idx) + ".png", format="PNG", dpi=150)
-    plt.cla()
+    ax2.set_ylim(-50, 150)
+    ax2.set_title('(Max Obs - Min Obs) - Reflectance ' + file)
+    ax2.grid()
+    ax2.set_xlabel('Wavelength[nm]')
+    ax2.set_ylabel('Reflectance[%]')
+    ax2.plot(wavelength1, reflectance_neg, 'k')
+    fig2.savefig("30deg_spectral_reflectance_neg" + str(idx) + ".png", format="PNG", dpi=150)
+    ax2.cla()
+
+
+    ax1.plot(wavelength1, reflectance_avg, c=np.random.rand(3,), label=file)
+
+#legend(loc=1, prop={'size': 6}
+ax1.legend()
+fig1.savefig("30deg_spectral_reflectance_all.png", format="PNG", dpi=150)
+
+
 
 
 
